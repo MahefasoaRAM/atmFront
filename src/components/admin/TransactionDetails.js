6b7cf03e69af9261ae73e63d250a39cb6c812834
MahefasoaRAM <andriatsilavomahefasoa@gmail.com>
2023-04-16 13:52:03 +0300

    second

--- src/components/admin/TransactionDetails.js
+++ src/components/admin/TransactionDetails.js
@@ -1,50 +1,54 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function TransactionDetails(props) {
-  const [loading, setLoading] = useState(true)
-  const [test, setTest] = useState([])
-  const id = props.match.params.id
+    const [loading, setLoading] = useState(true)
+    const [transaction, setTransaction] = useState(null)
 
-
-  useEffect(() => {
-    fetch(id)
-  }, [id])
-
-  const fetch = (id) => {
-    axios.get(`/api/transactiondetails/${id}`).then(res => {
-      if(res.data.status === 200){
-        console.log(id)
-        console.log(res.data.transactiondetails)
-        setTest(res.data.transactiondetails)
-        setLoading(false)
-        console.log(test)
-      }      
+    useEffect(() => {
+        const id = props.match.params.id
+        axios.get(`/api/transactiondetails/${id}`).then(res => {
+            setTransaction(res.data.transactiondetails)
+            setLoading(false)
+        }, [props.match.params.id])
     })
-  }
 
-  if(loading){
-    return (
-      <div className="d-flex justify-content-center container py-5">
-        <div className="spinner-border text-primary my-5" role='status'></div>
-      </div>
-    )
-  }
+    if(loading){
+        return (
+          <div className="d-flex justify-content-center container py-5">
+            <div className="spinner-border text-primary my-5" role='status'></div>
+          </div>
+        )
+      }
+
+      let date = new Date(transaction.created_at)
 
   return (
-    <div className='container mt-5'>
-    <div className='d-flex flex-row-reverse'>
-        <Link to='/adminatm/transaction' className='btn btn-info'>Back</Link>
-      </div>
-      <div className="card">
-        <div className="card-body">
-          {/* <h1 className="text-center text-capitalize">{transaction.naturetransaction} du {transaction.created_at}</h1>
-          <p className="card-title">De la part de {transaction.sender.name}  pour {transaction.receiver.name}</p>
-          <p>La valeur de {transaction.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")} Ar</p> */}
+    <div className="container py-5">
+    <div className='row justify-content-center'>
+      <div className="col-md-6 py-5">
+        <h1 className='text-center text-capitalize mb-4'>details transaction</h1>
+        <div className="card">
+          <div className="card-body">
+            <form>
+              <div className="form-group mb-3">
+                <label className="text-capitalize">{transaction.naturetransaction.name} du {date.toLocaleDateString()}</label>
+              </div>
+              <div className="form-group mb-3">
+                <label>De la part de : {transaction.sender.name}</label>
+              </div>
+              <div className="form-group mb-3">
+                <label className="text-capitalize">pour : {transaction.receiver.name}</label>
+              </div>
+              <div className="form-group mb-3">
+                <label>d'une valeur de : {transaction.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")} Ar</label>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
+  </div>
   )
 }
 
