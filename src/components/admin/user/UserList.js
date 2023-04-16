1c28e6bfb293f119424670089e78a5bb238554eb
MahefasoaRAM <andriatsilavomahefasoa@gmail.com>
2023-04-16 11:05:42 +0300

    second

--- src/components/admin/user/UserList.js
+++ src/components/admin/user/UserList.js
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom'
 function UserList() {
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState([])
-
-  useEffect(() => {
-    fetch()
+  
+  useEffect(() =>{
+    fetchdata()
   }, [])
 
-  const fetch = () => {
+  const fetchdata = () => {
     axios.get('/api/userlist').then(res => {
-      setUser(user)
+      setUser(res.data.user)
       setLoading(false)
     })
   }
@@ -22,6 +22,7 @@ function UserList() {
       fetch()
     })
   }
+
 
   if(loading){
     return (
@@ -33,42 +34,42 @@ function UserList() {
 
   return (
     <div className='container mt-5'>
-      <div className='d-flex flex-row-reverse mb-3'>
-        <Link to='/adminatm/useradd' className='btn btn-info'>Add user</Link>
-      </div>
-      <div className="card">
-        <div className="card-body">
-          <table className="table table-bordered">
-            <thead>
-              <tr className="text-center text-capitalize">
-                <th>name</th>
-                <th>email</th>
-                <th>phone</th>
-                <th>account number</th>
-                <th>balance</th>
-                <th>actions</th>
+    <div className='d-flex flex-row-reverse mb-3'>
+      <Link to='/adminatm/useradd' className='btn btn-info'>Add user</Link>
+    </div>
+    <div className="card">
+      <div className="card-body">
+        <table className="table table-bordered">
+          <thead>
+            <tr className="text-center text-capitalize">
+              <th>name</th>
+              <th>email</th>
+              <th>phone</th>
+              <th>account number</th>
+              <th>balance</th>
+              <th>actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {user.map(item => (
+              <tr className="text-catipatlize text-center" key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                <td>{item.account}</td>
+                <td>{item.balance}</td>
+                <td className='d-flex justify-content-around'>
+                  <Link to={`/adminatm/userdetails/${item.id}`} className='btn btn-info'>show</Link>
+                  <Link to={`/adminatm/useredit/${item.id}`} className='btn btn-warning'>edit</Link>
+                  <button onClick={() => userdelete(item.id)} className='btn btn-danger'>delete</button>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {user.map(item => (
-                <tr className="text-catipatlize text-center" key={item.id}>
-                  <td>{item.name}</td>
-                  <td>{item.email}</td>
-                  <td>{item.phone}</td>
-                  <td>{item.account}</td>
-                  <td>{item.balance}</td>
-                  <td className='d-flex justify-content-around'>
-                    <Link to={`/adminatm/userdetails/${item.id}`} className='btn btn-info'>show</Link>
-                    <Link to={`/adminatm/useredit/${item.id}`} className='btn btn-warning'>edit</Link>
-                    <Link onClick={() => userdelete(item.id)} classname='btn btn-danger'>delete</Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
+  </div>
   )
 }
 
